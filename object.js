a25680dc7fe5810005c44dfe6bf89f0631499857
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-03-01 19:09:00 +0600

    object explore

---
+++ object.js
@@ -0,0 +1,23 @@
+const user = [];
+user.name = 'Adnan';
+user.ocupation = 'Programmer';
+
+const Programmer = {
+   name: 'Hassan',
+   job: 'programmer',
+   salary: 10000
+};
+
+const user = new object();
+
+
+const item = object.create(null);
+
+/**
+ * way to create object 
+ * object literl
+ * object construcctor
+ * object constructor function
+ * Es6 classes
+ * object.create()
+ * */ 
